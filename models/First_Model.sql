b4ceb0e42bfc6f8d8d8d8e81ce8c21bbc411da8d
Picklu Ghosh <gpicklu21@gmail.com>
2026-03-05 18:40:38 +0000

    Adding the first model

---
+++ models/First_Model.sql
@@ -0,0 +1,8 @@
+SELECT
+    customer_id,
+    customer_name,
+    signup_date,
+    UPPER(customer_name) AS name_upper,
+    YEAR(signup_date) AS signup_year,
+    MONTH(signup_date) AS signup_month
+FROM {{ source('raw', 'customers') }}
