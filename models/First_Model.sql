9bbaff07f6a459416f159fe49eaf01dec23c3c8f
picklu ghosh <gpicklu21@gmail.com>
2026-03-06 18:05:40 +0000

    Adding the stage layer

--- models/First_Model.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    customer_id as customer_id,
-    customer_name as customer_name,
-    signup_date as signup_date ,
-    UPPER(customer_name) AS name_upper,
-    YEAR(signup_date) AS signup_year,
-    MONTH(signup_date) AS signup_month
-FROM {{ source('raw', 'customers') }}
