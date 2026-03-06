b50c9435d9b96fb6d2816f1ad1d7642d872dab1e
picklu ghosh <gpicklu21@gmail.com>
2026-03-06 11:13:38 +0000

    Testing the CICD

--- models/First_Model.sql
+++ models/First_Model.sql
@@ -1,7 +1,7 @@
 SELECT
     customer_id,
     customer_name as customer_name,
-    signup_date,
+    signup_date as signup_date ,
     UPPER(customer_name) AS name_upper,
     YEAR(signup_date) AS signup_year,
     MONTH(signup_date) AS signup_month
