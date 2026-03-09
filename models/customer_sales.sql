29dfe4e689cd6bb50ced7e3fe2256db5652bbe88
picklu ghosh <gpicklu21@gmail.com>
2026-03-09 16:19:56 +0000

    TEST

--- models/customer_sales.sql
+++ models/customer_sales.sql
@@ -4,7 +4,7 @@ SELECT
     c.CUSTOMER_NAME,
     c.SEGMENT,
     COUNT(DISTINCT o.ORDER_ID) AS TOTAL_ORDERS,
-    SUM((oi.QUANTITY * oi.UNIT_PRICE) - oi.DISCOUNT_AMOUNT) AS TOTAL_SP
+    SUM((oi.QUANTITY * oi.UNIT_PRICE) - oi.DISCOUNT_AMOUNT) AS TOTAL_SPENT
 FROM {{ source('ecommerce','fact_orders') }} o
 JOIN {{ source('ecommerce','fact_order_items') }} oi
     ON o.ORDER_ID = oi.ORDER_ID
