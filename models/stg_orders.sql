2fcefde66017908eef71ae33a38f416a932a6f59
picklu ghosh <gpicklu21@gmail.com>
2026-03-09 05:50:21 +0000

    CChanged column name of payment

--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -7,6 +7,6 @@ SELECT
     CHANNEL_KEY,
     PROMO_KEY,
     ORDER_STATUS,
-    PAYMENT_METHOD
+    PAYMENT_METHOD as mode
 FROM {{ source('ecommerce','fact_orders') }}
 WHERE ORDER_STATUS = 'Completed'
