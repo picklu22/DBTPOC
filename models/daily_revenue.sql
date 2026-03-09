741f61fb18dca8937bcd20caf6b08e715f7937d9
picklu ghosh <gpicklu21@gmail.com>
2026-03-09 11:15:12 +0000

    Test

--- models/daily_revenue.sql
+++ models/daily_revenue.sql
@@ -2,7 +2,7 @@
 
 SELECT
     d.DATE_VALUE,
-    SUM((oi.QUANTITY * oi.UNIT_PRICE) - oi.DISCOUNT_AMOUNT) AS DAILY_REVENUE
+    SUM((oi.QUANTITY * oi.UNIT_PRICE) - oi.DISCOUNT_AMOUNT) AS DAILY_REV
 FROM {{ source('ecommerce','fact_orders') }} o
 JOIN {{ source('ecommerce','fact_order_items') }} oi
     ON o.ORDER_ID = oi.ORDER_ID
