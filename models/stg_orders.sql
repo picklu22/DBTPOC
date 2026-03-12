75f3d2d48dc049afda1e0f54623938334bd29cfa
picklu ghosh <gpicklu21@gmail.com>
2026-03-12 01:59:17 +0000

    test

--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -7,6 +7,6 @@ SELECT
     CHANNEL_KEY,
     PROMO_KEY,
     ORDER_STATUS,
-    PAYMENT_METHOD as Mode
+    PAYMENT_METHOD as METHOD
 FROM {{ source('ecommerce','fact_orders') }}
 WHERE ORDER_STATUS = 'Completed'
